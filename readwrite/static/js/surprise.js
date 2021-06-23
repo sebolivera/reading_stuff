ff1301fa2abf47c6bd0aa340bfb10c5d203b5beb
solivera <solivera@matrice.io>
2021-06-23 21:16:59 +0200

    you don't want to know

--- readwrite/static/js/surprise.js
+++ readwrite/static/js/surprise.js
@@ -1,10 +1,22 @@
 // jGravity works best when targeting the body
 $(document).ready(function()
 {
+    setTimeout(() =>
+        {$(".angry").fadeIn(5000).delay(2000).css("filter", "saturate(255)");
+    }, 5000 );
+        
     $("#please-dont-click").on("click", function()
     {
-        $('#under-const').throwable({ 
-            gravity:{x:0,y:10},
+        // $(".angry").delay(2000).css("transition", "saturate 5s");
+        $('.card, .nav-link, .navbar, footer, header').each(function()
+        {
+            let i = Math.floor(Math.random() * 5000);
+            //$(this).throwable({gravity:{x:0,y:10},}).delay(Math.floor(Math.random() * 10000)+1000);
+            setTimeout(() =>
+            {
+                $(this).throwable({gravity:{x:0,y:10},})
+            }, i * 2 );
+            
         });
     });
 });
