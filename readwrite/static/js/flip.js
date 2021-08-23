52ae8885ee99a9cc8b0b1ca4877d4c64830977c7
solivera <solivera@matrice.io>
2021-08-23 20:16:25 +0200

    some details added to the login page

--- readwrite/static/js/flip.js
+++ readwrite/static/js/flip.js
@@ -16,6 +16,15 @@ $(document).ready(function(){
         $(".unmirror").first().removeClass("unmirror");
         $("*").removeClass("text-light").removeClass("text-dark");
         $("#ohno").remove();
-    });
+        $("nav").css("visibility", "hidden");
+        $("footer").remove();
+        $("body").removeClass("bg-dark");
+        $("body").removeClass("bg-light");
+        $("html").css("background", "url("+m_image+") no-repeat");
+        $("html").css("background-position", "center");
+        $("html").css("background-size", "cover");
+        $("body").css("background-color", "rgba(38, 0, 0, 0.97)");
+        $("html, body").css("height", "100%");
+    }).delay( 1000 );
     
 });
