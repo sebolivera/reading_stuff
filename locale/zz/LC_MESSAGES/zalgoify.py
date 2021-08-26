061c3496a44271a1865078dac47ced001aac3ae7
solivera <solivera@matrice.io>
2021-08-27 00:45:55 +0200

    this is starting to get cursed

---
+++ locale/zz/LC_MESSAGES/zalgoify.py
@@ -0,0 +1,33 @@
+from zalgo_text import zalgo
+import os 
+import random
+
+def random_unicode(length):
+    # Create a list of unicode characters within the range 0000-D7FF
+    random_unicodes = [chr(random.randrange(0x3300)) for _ in range(0, length)] 
+    return u"".join(random_unicodes)
+
+my_random_unicode_str = random_unicode(length=512)
+my_random_utf_8_str = my_random_unicode_str.encode('utf-8')
+
+d = os.path.dirname(os.path.realpath(__file__))
+newlines = []
+with open(d+'/django.po') as f:
+    l = f.readline()
+    while l:
+        if l[0:5]=="msgid":
+            b = l.find('"')+1
+            e = l.rfind('"')
+            if b+1<e:
+                newlines.append(l)
+                l = f.readline()
+                l = 'msgstr "'+zalgo.zalgo().zalgofy(random_unicode(e-b))+'"\n'
+        newlines.append(l)
+        l = f.readline()
+    newlines.append(l)
+
+
+with open(d+"/django_new.po", "a+") as document1:
+        document1.writelines(newlines)
+
+os.replace(d+"/django_new.po", d+"/django.po")
--- reading_site/urls.py
+++ reading_site/urls.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('', include('userauth.urls')),
-    path('', include('readwrite.urls')),
+    path('', include('readwrite.urls')), #last bc posts can have any url atm 
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# urlpatterns += i18n_patterns(
+#     path('', include('userauth.urls')),
+#     path('', include('readwrite.urls')),
+#     path('ckeditor/', include('ckeditor_uploader.urls')),
+#     prefix_default_language=False
+#  ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
