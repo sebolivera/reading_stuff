0c6c3fcb44363637dadc8292d8357d596e0331f9
solivera385 <solivera@matrice.io>
2022-02-25 18:27:41 +0100

    custom 404(

--- reading_site/settings.py
+++ reading_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.0', 'reading-stuff.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'reading-stuff.herokuapp.com']
 
 
 # Application definition
